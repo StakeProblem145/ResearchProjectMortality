36e36558858c1e7ad19def59a6bc0edde252d3bd
StakeProblem145 <elbarto145@hotmail.de>
2022-04-20 19:47:49 +0200

    Working DT

--- ItalyClassicModels.R
+++ ItalyClassicModels.R
@@ -1,12 +1,13 @@
 library(StMoMo)
 library(demography)
+library(tidyverse)
 library(rpart)
+library(rpart.plot)
 
 
-
-ITALYdataRaw <- hmd.mx(country="ITA", username = "lstake@hs-koblenz.de", password = "1650394322")
-ITALYdata
-plot(ITALYdata, series="male")
+#ITALYdataRaw <- hmd.mx(country="ITA", username = "lstake@hs-koblenz.de", password = "1650394322")
+load("data/raw/ITALYdataRaw.RDA")
+load("data/processed/HMD_df.RDA")
 
 #Ages and gender for fitting
 ITALYdata <- StMoMoData(ITALYdataRaw, series = "male")
@@ -37,13 +38,15 @@ plot(RHRes)
 ### DT ###
 dx_mdl <- LCFit$Ext * exp(LCFit$ax + LCFit$bx %*% LCFit$kt)
 
-year <- LCFit$years
-age <- LCFit$ages
-cohort <- LCFit$cohorts
+HMD_df <- filter(HMD_df, Year >= years.fit[1] & Year <= years.fit[length(years.fit)] & Gender == "Male")
+
+HMD_df$dx <- as.vector(t(dx_mdl))
+HMD_df <- HMD_df %>%
+  mutate("psi" = Deaths/dx)
 
 
-
-
-dt <- rpart(LCFit$Dxt/dx_mdl ~ Age + Year,
+dt <- rpart(psi ~ Age + Year,
             data = HMD_df, method = "poisson",
             cp = 0.003)
+
+prp(dt)

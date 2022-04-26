034c9594812caaa89b5fb28785e04872a5bcea95
StakeProblem145 <elbarto145@hotmail.de>
2022-04-26 09:55:35 +0200

    Syntax Improvements

--- ItalyClassicModels.R
+++ ItalyClassicModels.R
@@ -12,21 +12,21 @@ load("data/processed/HMD_df.RDA")
 
 #Ages and Years for Fitting and Forecasting
 ITALYdata <- StMoMoData(ITALYdataRaw, series = "male")
-ages.fit <- 30:100
-years.fit <- 1981:2019
+ages_fit <- 30:100
+years_fit <- 1981:2019
 #prep for forecast 2009-2019
-years.subset_forecast <- 1981:2009
+years_subset_forecast <- 1981:2009
 
 #Base Models
 LCModel <- lc(link = "log")
 APCModel <- apc(link = "log")
 
 #Base Model Fits
-LCFit <- fit(LCModel, data = ITALYdata, ages.fit = ages.fit, years.fit = years.fit)
-LCFit.subset_forecast <- fit(LCModel, data = ITALYdata, ages.fit = ages.fit, years.fit = years.subset_forecast)
+LCFit <- fit(LCModel, data = ITALYdata, ages.fit = ages_fit, years.fit = years_fit)
+LCFit.subset_forecast <- fit(LCModel, data = ITALYdata, ages.fit = ages_fit, years.fit = years_subset_forecast)
 
-APCFit <- fit(APCModel, data = ITALYdata, ages.fit = ages.fit, years.fit = years.fit)
-APCFit.subset_forecast <- fit(APCModel, data = ITALYdata, ages.fit = ages.fit, years.fit = years.subset_forecast)
+APCFit <- fit(APCModel, data = ITALYdata, ages.fit = ages_fit, years.fit = years_fit)
+APCFit.subset_forecast <- fit(APCModel, data = ITALYdata, ages.fit = ages_fit, years.fit = years_subset_forecast)
 
 ### Plot Fit 
 plot(LCFit)
@@ -47,35 +47,37 @@ APCFor <- forecast(APCFit.subset_forecast, h=10)
 plot(APCFor, parametricbx = FALSE)
 
 #Preparation of data
-dx_mdl <- LCFit$Ext * exp(LCFit$ax + LCFit$bx %*% LCFit$kt)
+choosen_Model <- APCFit
 
-HMD_df <- filter(HMD_df, Year >= years.fit[1] & Year <= years.fit[length(years.fit)] & Gender == "Male")
+dx_mdl <- choosen_Model$Ext * exp(choosen_Model$ax + choosen_Model$bx %*% choosen_Model$kt)
 
-HMD_df$dx <- as.vector(t(dx_mdl))
-HMD_df <- HMD_df %>%
+HMD_df_filtered <- filter(HMD_df, Year >= years_fit[1] & Year <= years_fit[length(years_fit)] & Age >= ages_fit[1] & Age <= ages_fit[length(ages_fit)] & Gender == "Male")
+
+HMD_df_filtered$dx <- as.vector(t(dx_mdl))
+HMD_df_filtered <- HMD_df_filtered %>%
   mutate("psi" = Deaths/dx, "Cohort" = Year - Age)
 
 ### DT ###
 dt <- rpart(psi ~ Age + Year + Cohort,
-            data = HMD_df, method = "poisson",
-            cp = 0.0003)
+            data = HMD_df_filtered, method = "poisson",
+            cp = 0.003)
 
 prp(dt)
 
 ### RF ###
-rf <- randomForest(formula = Age + Year + Cohort~., data = HMD_df, ntree=200)
+rf <- randomForest(formula = Age + Year + Cohort~., data = HMD_df_filtered, ntree=200)
 plot(rf, type="l")
-rf1 <- randomForest(formula = Age + Year~., data = HMD_df, ntree=300)
+rf1 <- randomForest(formula = Age + Year~., data = HMD_df_filtered, ntree=300)
 plot(rf1, type="l")
 
 ### GB ###
-gbm1 <- gbm(psi ~ Age + Year + Cohort,  "gaussian", data = HMD_df, n.trees = 5000, interaction.depth = 6, 
+gbm1 <- gbm(psi ~ Year + Age + Cohort,  "gaussian", data = HMD_df_filtered, n.trees = 5000, interaction.depth = 6, 
             shrinkage = 0.001,  cv.folds = 5)
 tree_1 <- pretty.gbm.tree(gbm1, i.tree = 1)
 plot.gbm(gbm1,1:2,5)
 plot.gbm(gbm1,1:2,50)
-plot.gbm(gbm1,1:3,500)
-plot.gbm(gbm1,1:3,5000)
+plot.gbm(gbm1,1:2,500)
+plot.gbm(gbm1,1:2,5000)
 summary.gbm(gbm1, n.trees = 1)
 
 
